b1d2f88d252017f49d62e7fdc940e589966ca6c4
sifat ullah <sifatullah@Sifat-Macbook.local>
2018-10-26 17:25:51 +0500

    sifat changes 3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1> i am sifat ullah shah</h1>
+          <h1> i am sifat ullah shah </h1>
         </header>
       </div>
     );
