2cb7b3226be521d1897df7985062fdc8371ea607
Malik Ahsan <malikahsan@Maliks-MacBook-Pro.local>
2018-10-26 16:47:52 +0500

    Changed app file

--- src/App.js
+++ src/App.js
@@ -7,19 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Codematics
-            
-          </a>
+          <h1> Here is some code</h1>
         </header>
       </div>
     );
